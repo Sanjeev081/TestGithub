8aac8b3bd0c512986893a54e09878eeb7df5a1cd
Sanjeev081 <sanjeevkumar12345.sk@gmail.com>
2020-09-02 14:54:50 +0530

    added search page feature

--- src/main/java/com/qa/igspage/Loginpage.java
+++ src/main/java/com/qa/igspage/Loginpage.java
@@ -1,5 +1,11 @@
 package com.qa.igspage;
 
 public class Loginpage {
+	public void loginpage() {
+		System.out.println("loginpage");
+	}
+	public void loginset() {
+		System.out.println("login set");
+	}
 
 }
